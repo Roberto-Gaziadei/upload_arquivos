c44067efa9a47f6865472dac4d4bf326eaaa72ca
Roberto_Graziadei <roberto.2022315930@aluno.iffar.edu.br>
2024-05-28 17:14:23 -0300

    Update upload.php
    ..

--- upload.php
+++ upload.php
@@ -2,8 +2,17 @@
 
 $pastaDestino = "uploads/";
 $nomeArquivo = $_FILES['arquivo']['name'];
-Var_dump($_FILES['arquivo']['name']);
+Var_dump($_FILES);
+var_dump($_FILES['arquivo']['size']);
 
-file_exists($pasteDestino . $nomeArquivo);
+//Verificar se o arquivo é maior que 2MB
+if($_FILES['arquivo']['size'] > 2000000 ){
+    echo "O tamanho do arquivo é maior que o limite permitido.";
+    die();
+}else{
+    echo "";
+}
+
+//file_exists($pasteDestino . $nomeArquivo);
 
 ?>
